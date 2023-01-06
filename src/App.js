99fd20519d33e4b7261ce375fbc2784a67fb506f
Utpal <utpalk904@gmail.com>
2023-01-06 23:59:42 +0530

    addedcomponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-import FloatingTextInput from './Components/FloatInput';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import PageNotFound from './Pages/PageNotFound';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <FloatingTextInput inputType="text" InputId="username" InputName="username" InputPlaceholder="Enter your username" InputLabel="Username" />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<Home />} />
+        <Route path='/contact-us' element={<Home />} />
+        <Route path='/login' element={<Home />} />
+        <Route path='/register' element={<Home />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
